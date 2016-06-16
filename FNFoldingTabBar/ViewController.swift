632e615bc7709c649c2132e596e70adcf86f119d
LuWeijun <luweijun@weidian.com>
2016-06-16 10:55:11 +0800

    Initial Commit

---
+++ FNFoldingTabBar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FNFoldingTabBar
+//
+//  Created by Fnoz on 16/6/16.
+//  Copyright © 2016年 Fnoz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
